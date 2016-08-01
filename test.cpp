efc47ec383f0efd8a06372b773bbc43dbc504618
Cooler_ <coolerlair@gmail.com>
2016-08-01 14:15:58 -0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -13,30 +13,29 @@ int main(int argc, char* argv[])
 
   robot.set_training_data_file(std::string("training_quotes3.txt"));
 
-  robot.add_training_data("compre agora promoção SPAM");
-  robot.add_training_data("preço super barato SPAM");
-  robot.add_training_data("aproveite agora somente nesta sexta SPAM");
-  robot.add_training_data("ola meu amigo como vai você ? OK");
-  robot.add_training_data("pagamento efetuado OK");
-  robot.add_training_data("esta tudo certo velho amigo OK");
-  robot.add_training_data("compre agora promoção SPAM");
-  robot.add_training_data("preço super barato SPAM");
-  robot.add_training_data("aproveite agora somente nesta sexta SPAM");
-  robot.add_training_data("ola meu amigo como vai você ? OK");
-  robot.add_training_data("pagamento efetuado OK");
-  robot.add_training_data("esta tudo certo velho amigo OK");
-  robot.add_training_data("compre agora promoção SPAM");
-  robot.add_training_data("preço super barato SPAM");
-  robot.add_training_data("aproveite agora somente nesta sexta SPAM");
-  robot.add_training_data("ola meu amigo como vai você ? OK");
-  robot.add_training_data("pagamento efetuado OK");
-  robot.add_training_data("esta tudo certo velho amigo OK");
-  robot.add_training_data("compre agora promoção SPAM");
-  robot.add_training_data("preço super barato SPAM");
-  robot.add_training_data("aproveite agora somente nesta sexta SPAM");
-  robot.add_training_data("ola meu amigo como vai você ? OK");
-  robot.add_training_data("pagamento efetuado OK");
-  robot.add_training_data("esta tudo certo velho amigo OK");
+  robot.add_training_data("Hot girls for money SPAM");
+  robot.add_training_data("nude girls for you SPAM");
+  robot.add_training_data("more sexy for 24hours SPAM");
+  robot.add_training_data("hey brow how are you ? OK");
+  robot.add_training_data("do you have a OK");
+  robot.add_training_data("you can help me cheers OK");
+  robot.add_training_data("buy new car only today SPAM");
+  robot.add_training_data("price of cars is low SPAM");
+  robot.add_training_data("bangbus photos click here to access SPAM");
+  robot.add_training_data("credit suisse OK");
+  robot.add_training_data("paypal payment  OK");
+  robot.add_training_data("my friend hello OK");
+  robot.add_training_data("black friday SPAM");
+  robot.add_training_data("tits and ass sex girls SPAM");
+  robot.add_training_data("milfs of the sex day SPAM");
+  robot.add_training_data("linux openbsd freebsd cpp programming coding code OK");
+  robot.add_training_data("ibovespa iboc nasdaq nysy jones hedge gold OK");
+  robot.add_training_data("you have new coin OK");
+  robot.add_training_data("womans for sex SPAM");
+  robot.add_training_data("you won bilion milion SPAM");
+  robot.add_training_data("you have won milion SPAM");
+  robot.add_training_data("my friends name OK");
+  robot.add_training_data("its ok is not a joke here OK");
 
   robot.train();
 
